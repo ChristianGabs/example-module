5b644c670d58039b883d810352dac3e3607e9ee5
ChristianGabs <developer.webservice@gmail.com>
2024-06-26 22:51:31 +0100

    Add console example for info message

---
+++ src/Console/ExampleInfo.php
@@ -0,0 +1,61 @@
+<?php
+
+/**
+ * FOSSBilling.
+ *
+ * @copyright FOSSBilling (https://www.fossbilling.org)
+ * @license   Apache-2.0
+ *
+ * Copyright FOSSBilling 2022
+ * This software may contain code previously used in the BoxBilling project.
+ * Copyright BoxBilling, Inc 2011-2021
+ *
+ * This source file is subject to the Apache-2.0 License that is bundled
+ * with this source code in the file LICENSE
+ */
+
+namespace Box\Mod\Example\Commands;
+
+use Pimple\Container;
+
+use CristianG\PimpleConsole\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class ExampleInfo extends Command implements \FOSSBilling\InjectionAwareInterface
+{
+    protected ?Container $di = null;
+
+    /**
+     * @param  Container  $di
+     * @return void
+     */
+    public function setDi(Container $di): void
+    {
+        $this->di = $di;
+    }
+
+    /**
+     * @return Container|null
+     */
+    public function getDi(): ?Container
+    {
+        return $this->di;
+    }
+
+    /**
+     * @return void
+     */
+    protected function configure(): void
+    {
+        $this->setName('example:info');
+        $this->setDescription('Return an info message');
+        parent::configure();
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $this->info("This is your example info");
+        return Command::SUCCESS;
+    }
+}
